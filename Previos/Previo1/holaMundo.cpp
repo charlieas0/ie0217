7400e09bab4afb823dfc70e4df9e0da949153016
Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>
2024-03-14 20:49:36 -0600

    Agregando Previos
    Signed-off-by: Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>

---
+++ Previos/Previo1/holaMundo.cpp
@@ -0,0 +1,8 @@
+// your first C++ program
+
+#include<iostream>
+int main (){
+    std::cout << "hola a todos y todas, estudiantes IE0217 \n ";
+    std::cout << "Probando  el stash\n ";
+    return 0;
+}
