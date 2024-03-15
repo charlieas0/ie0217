0d432555b09a30f8eb28dab3754069f9bd0d5776
Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>
2024-03-14 20:56:57 -0600

    Agregando Previo2, con ejemplos de C++
    Signed-off-by: Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>

---
+++ Previos/Previo2/holaMundo.cpp
@@ -0,0 +1,18 @@
+// Esto es un programa propio 
+#include<iostream> // para mostrar o poder imprimir en pantalla.
+using namespace std;
+
+
+// Generando el main.
+int main(){
+    cout << "Hola mundo, estudiantes de IE0217 \n";
+
+    int var1 = 0; // instanciando variables
+    int var2;
+    
+    var2 = (var1 == 3 ) ? 15: 27; // utilizacion del operador ternario "?"
+
+    cout << "var2: " << var2 << ""; // Imprimiendo var2.
+    cout << "\n"; // cambio de linea
+    return 0;
+}
