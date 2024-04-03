a3cda9da7d4e18bd3c1450e9a0740da81ff4269a
Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>
2024-04-03 10:39:35 -0600

    Documentando archivos
    Signed-off-by: Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>

--- Tareas/TAREA_UNO/src/ahorcado.hpp
+++ Tareas/TAREA_UNO/src/ahorcado.hpp
@@ -1,8 +1,9 @@
-/*
-Charlie Arroyo
-B90765
-UCR
-
+/**
+ * @file ahorcado.hpp
+ * @version 1.0
+ * @date 08/04/2024
+ * @author Charlie Arroyo 
+ * @brief Funciones de programa.
 */
 
 #ifndef AHORCADO_HPP
@@ -11,6 +12,9 @@ UCR
 #include <string>
 #include<iostream>
 // Generando el struct.
+/**
+ * @brief Ahorcado Estructura del programa de ahorcado.
+*/
 struct Ahorcado {
     std::string palabra_adivinar;
     std::string est_actual;
@@ -19,10 +23,28 @@ struct Ahorcado {
 };
 
 // Declarando las funciones..
+/**
+ * @brief inicializar Se inicializa el programa
+*/
 void inicializar(Ahorcado& juego, const std::string palabras[], int numPalabras, int intento_max);
+/**
+ * @brief AdivinarLetra El usuario adivina o no la palabra
+*/
 void adivinarLetra(Ahorcado& juego, char letra);
+/**
+ * @brief verificarAdivinanza
+*/
 bool verificarAdivinanza(Ahorcado& juego);
+/**
+ * @brief jugarJuego Funcion para jugarlo
+*/
 void jugarJuego(Ahorcado& juego);
+/**
+ * @brief mostrarMenu Muentra el menu en pantalla
+*/
 void mostrarMenu();
+/**
+ * @brief menu Menu iterativo
+*/
 void menu(Ahorcado& juego,int& intentos_maximos, std::string palabras[], int& numPalabras);
 #endif 
