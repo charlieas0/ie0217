9bff85986a89ae35cd55e30a0794da3d32c3d800
Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>
2024-04-03 09:17:22 -0600

    Definiendo funciones de pantalla
    Signed-off-by: Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>

--- Tareas/TAREA_UNO/pantalla.cpp
+++ Tareas/TAREA_UNO/pantalla.cpp
@@ -7,7 +7,7 @@ UCR
 #include "pantalla.hpp"
 #include <iostream>
 
-// Generando una funcion para limpiar la pantalla
+// Generando una funcion para limpiar la pantalla.
 void limpiar() {
     if (system("clear") == -1) {
         std::cout << "Error al limpiar pantalla." << std::endl;
