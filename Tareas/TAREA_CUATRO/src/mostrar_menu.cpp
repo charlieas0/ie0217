7e3193874adb1efb16e2010f6aa143fcbeca8eb4
Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>
2024-05-08 11:01:18 -0600

    Archivos de encabezado para mostrar menu
    Signed-off-by: Charlie Arroyo S <charlie.arroyo@ucr.ac.cr>

---
+++ Tareas/TAREA_CUATRO/src/mostrar_menu.cpp
@@ -0,0 +1,15 @@
+#include "mostrar_menu.hpp" // Incluye el archivo de encabezado
+
+// Definición de la función mostrarMenu()
+void mostrarMenu() {
+    // Muestra el menú en la consola
+    std::cout << "\n--- Menú ---" << std::endl;
+    std::cout << "1. Ingresar tamaño y valores de las matrices" << std::endl;
+    std::cout << "2. Seleccionar tipo de datos para las matrices" << std::endl;
+    std::cout << "3. Generar datos aleatorios para las matrices" << std::endl;
+    std::cout << "4. Mostrar las matrices ingresadas" << std::endl;
+    std::cout << "5. Seleccionar operación a realizar" << std::endl;
+    std::cout << "6. Ejecutar la operación" << std::endl;
+    std::cout << "7. Salir" << std::endl;
+    std::cout << "Ingrese su opción: ";
+}
---
+++ Tareas/TAREA_CUATRO/src/mostrar_menu.hpp
@@ -0,0 +1,11 @@
+#ifndef MOSTRAR_MENU_HPP
+#define MOSTRAR_MENU_HPP
+
+#include <iostream>
+
+/**
+ * @brief Función para mostrar el menú en la consola.
+ */
+void mostrarMenu();
+
+#endif 
